6636bdf9e0bf4e25d89b4175f842a9898b0512e7
architmaurya <architmaurya7054@gmail.com>
2024-11-22 11:53:36 +0530

    New module is added in the file as usermodels

---
+++ Backend/models/usermodels.js
@@ -0,0 +1,12 @@
+const mongoose=require("mongoose")
+const UserSchema=new mongoose.Schema({
+    name:{type:String,required:true},
+    email:{type:String,required:true},
+    contact:{type:Number,required:true},
+    password:{type:String,required:true},
+    img:{type:String,required:true},
+    createdAt:{type:Date,default:Date.now()},
+    updatedAt:{type:Date,default:Date.now()}
+})
+const UserTable=mongoose.model("Users",UserSchema)
+module.exports={UserTable}
