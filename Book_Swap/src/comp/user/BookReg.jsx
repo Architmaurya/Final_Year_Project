01e9d2de4a8d3170185154ae8ba270ee6116b68d
architmaurya <architmaurya7054@gmail.com>
2024-11-22 10:30:12 +0530

    made the BookReg form and changes in the css file also

--- Book_Swap/src/comp/user/BookReg.jsx
+++ Book_Swap/src/comp/user/BookReg.jsx
@@ -1,19 +1,45 @@
 function BookReg(){
     return(
         <>
-        <div className="container">
-            <form action="">
-                <div className="row">
-                <div className="col-md-6">
-                      <div className="form-group">
-                          <label className="mb-2 ms-1 mt-1 form_label">Name :</label>
-                          <input  type="text" className="form-control" placeholder="Enter Your Name" />
-                        </div>
-                      </div>
-                </div>
-            </form>
+         <div className="container_Bookform">
+        <div className="row ">
+            <div className="col-lg-6 mx-auto text-center mt-3">
+            <h1 className="">Book<span className="text-primary"> Form</span> </h1>
+            </div>
         </div>
-        <h1>Archit</h1>
+            <div className="row ">
+
+                <div className="col-lg-5 mx-auto mt-4 mb-4 form-style-Book">
+                <form>
+                    <div className="">
+                      <input type="text" className="form-control mb-3 mt-3" placeholder="  Book_Name"/>
+                    </div>
+                    <div className="">
+                    <input type="text" className="form-control mb-3 " placeholder="Author Name"/>
+                    </div>
+                    <div className="">
+                      <input type="text" className="form-control mb-3 mt-3" placeholder="Genres"/>
+                    </div>
+                    <div className="">
+                    <input type="number" className="form-control mb-3" placeholder="Enter the Amount"/>
+                    </div>
+                    <div className="">
+                    <input type="moblie" className="form-control mb-3" placeholder="Enter Your Contact Number"/>
+                    </div>
+                    <div className="">
+                    <input type="file" className="form-control mb-3 " />
+                    </div>
+                    <div className="">
+                    <input type="submit" value="SUBMIT" className="form-control mb-3 fs-5 bg-primary" />
+                    </div>
+
+                </form>
+                </div>
+                {/* <div className="col-lg-5 mx-auto ">
+                    <img src="../../public/user.jpg" className="img-fluid"/>
+                </div> */}
+            </div>
+        </div>
         </>
     )
 }
